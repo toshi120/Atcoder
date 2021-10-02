3938488841c79698e56235f8e590f544fa32f04e
toshi120 <toshizy@gmail.com>
2021-10-02 16:33:51 +0900

    B - Shiritoriの回答追加

---
+++ B/B-Shiritori.rb
@@ -0,0 +1,19 @@
+n = gets.to_i
+w = readlines.map(&:chomp)
+ 
+cnt = 1
+if w.uniq == w 
+  (0..n-1).each do |i|
+    if i+1 != n
+      if w[i][w[i].length-1] == w[i+1][0]
+        cnt +=1
+      end
+    end
+  end
+end
+ 
+if cnt == n
+  puts "Yes"
+else
+  puts "No"
+end
